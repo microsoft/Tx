8ebbc3902ca145cc455725632160490328685c08
RuslanGabitov <ruslan.gabitov@live.com>
2014-01-21 10:23:15 -0800

    Added PerfCounterAverageAndDeviation method to Playback samples

--- Samples/Playback/Program.cs
+++ Samples/Playback/Program.cs
@@ -27,6 +27,7 @@ namespace TxSamples
             CountAllTwoFiles();
             CountAcrossHierarchies();
             Count5SecWindow();
+            PerfCounterAverageAndDeviation();
         }
 
         #region Structured Mode
@@ -223,6 +224,45 @@ namespace TxSamples
 
             countPerWindow.Subscribe(Console.WriteLine);
 
+            playback.Run();
+        }
+
+        static void PerfCounterAverageAndDeviation()
+        {
+            var playback = new Playback();
+            playback.AddPerfCounterTraces(@"BasicPerfCounters.blg");
+
+            var procTimeTotal = from ps in playback.GetObservable<PerformanceSample>()
+                                where ps.CounterSet == "Processor" && ps.CounterName == "% Processor Time" && ps.Instance == "_Total"
+                                select ps;
+
+            var powerSumBases = from ps in procTimeTotal
+                                select new
+                                {
+                                    s0_base = 1,
+                                    s1_base = ps.Value,
+                                    s2_base = ps.Value * ps.Value
+                                };
+
+            var powerSums =
+                from window in
+                    powerSumBases.Window(TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1), playback.Scheduler)
+                from a in window.Aggregate(new { s0 = 0, s1 = 0.0, s2 = 0.1 }, (acc, point) => new
+                {
+                    s0 = acc.s0 + point.s0_base,
+                    s1 = acc.s1 + point.s1_base,
+                    s2 = acc.s2 + point.s2_base
+                })
+                select a;
+
+            var avgAndDeviation = from ps in powerSums
+                                  select new
+                                  {
+                                      Average = ps.s1 / ps.s0,
+                                      Deviation = Math.Sqrt((ps.s0 * ps.s2 - ps.s1 * ps.s1) / (ps.s0 * ps.s0 - 1))
+                                  };
+
+            avgAndDeviation.Subscribe(value => Console.WriteLine("Average: {0}, Deviation: {1}", value.Average, value.Deviation));
             playback.Run();
         }
 
