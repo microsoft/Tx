aa18370ac273ee8def76488fbdec5404e93345e4
Sergey Baranchenkov <tuatan@yandex.ru>
2015-06-26 11:01:04 -0700

    Addded missing files

--- Source/Tx.Bond/BondEtwObserver.cs
+++ Source/Tx.Bond/BondEtwObserver.cs
@@ -5,7 +5,6 @@ namespace Tx.Bond
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
     using System.Threading;
 
---
+++ Source/Tx.Bond/Properties/Tx.Bond.Layout.cmd
@@ -0,0 +1,11 @@
+rd /s/q Tx.Bond
+
+md Tx.Bond\lib\Net40
+copy Net40\Tx.Bond.dll Tx.Bond\lib\Net40
+copy Net40\Tx.Bond.xml Tx.Bond\lib\Net40
+copy Net40\Tx.Bond.pdb Tx.Bond\lib\Net40
+
+md Tx.Bond\lib\Net45
+copy Net45\Tx.Bond.dll Tx.Bond\lib\Net45
+copy Net45\Tx.Bond.xml Tx.Bond\lib\Net45
+copy Net45\Tx.Bond.pdb Tx.Bond\lib\Net45
