173e38d57f4bcac129dfb5ee5c6b7a9220679327
jonmod <jonatham@microsoft.com>
2015-11-23 15:01:05 -0800

    small changes for snmpdatagram parsing.

--- Source/Tx.Network/SNMPDatagram.cs
+++ Source/Tx.Network/SNMPDatagram.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.DirectoryServices.Protocols;
+
 namespace Tx.Network
 {
 
-    enum UniversalClassTags: byte { 
+    enum UniversalClassTags : byte
+    {
 
-        
+
         EOC = 0,
         BOOLEAN = 1,
         INTEGER = 2,
@@ -39,7 +42,7 @@ namespace Tx.Network
         CHARACTER_STRING = 29,
         BMPString = 30
     }
-    
+
     class SNMPDatagram
     {
         struct Encoding_Structure_ShortLen
@@ -59,25 +62,25 @@ namespace Tx.Network
 
         struct IdByte
         {
-            byte Class;
-            byte PC;
-            byte TagNumber;
+            ClassBits classbits;
+            PC PC;
+            object TagNumber;
 
 
         }
 
-        struct ClassBits
+        enum ClassBits : byte
         {
-            byte Universal;
-            byte Application;
-            byte ContextSpecific;
-            byte Private;
+            Universal = 0,
+            Application,
+            ContextSpecific,
+            Private
         }
 
-        struct PC
+        enum PC : byte
         {
-            byte Primitive;
-            byte Constructed;
+            Primitive = 0,
+            Constructed = 1
         }
 
         struct LongTagNumbers
@@ -88,15 +91,15 @@ namespace Tx.Network
             byte LastOctet;
         }
 
-        struct LengthShortForm
+        struct DefLengthShortForm
         {
             //0,Octets count 7-1 (big endian?)
             byte Length;
         }
 
-        struct LengthLongForm
+        struct DefLengthLongForm
         {
-            //all 1's nto allowed;
+            //all 1's nto allowed; 
             byte FirstByte;
             byte[] Length;
 
@@ -109,8 +112,37 @@ namespace Tx.Network
             byte eocByte1;
             byte eocByte2;
         }
-        
+
+        ushort EOC = (ushort)0;
+
+        enum BERBool : int
+        {
+            BERTrue = 66047, //can be any non-zero vallue
+            BERFalse = 65792
+        }
+
+        static object MakeBERInt(int i)
+        {
+            var j = i >> 31 - 1 != 0 ? (~(i & (int.MaxValue >> 1))) + 1 : i;
 
 
+            return j;
+        }
+        static long MakeBERLong(long i)
+        {
+            var j = i >> 63 != 0 ? (~(i & (long.MaxValue >> 1))) + 1 : i;
+
+
+            return j;
+        }
+
+        static double MakeBERDouble(double i)
+        {
+            if (i == 0) return double.NaN;
+
+            throw new NotImplementedException();
+        }
+
+     
     }
 }
