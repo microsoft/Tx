3d65254d7660fc871d8bd695a1ebece3e041ac2d
Mark Mann <maman@microsoft.com>
2016-02-29 16:06:39 -0800

    Added attributes to enable parsing incoming SNMP traps into statically typed C# classes.

---
+++ Source/Tx.Network/Snmp/Dynamic/SnmpOidAttribute.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Tx.Network.Snmp.Dynamic
+{
+    using System;
+
+    /// <summary>
+    /// Attribute that marks a property that maps to an SNMP object.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class SnmpOidAttribute : Attribute
+    {
+        public ObjectIdentifier Oid { get; private set; }
+
+        /// <summary>
+        /// Constructs a new <see cref="SnmpOidAttribute"/> with the given object identifier.
+        /// </summary>
+        /// <param name="oid">The identifier of the object that is represented by the attributed property.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="oid"/> is null or whitespace.</exception>
+        public SnmpOidAttribute(string oid)
+        {
+            if (string.IsNullOrWhiteSpace(oid))
+            {
+                throw new ArgumentNullException("oid");
+            }
+
+            this.Oid = new ObjectIdentifier(oid);
+        }
+    }
+}
---
+++ Source/Tx.Network/Snmp/Dynamic/TrapTypeMap.cs
@@ -0,0 +1,181 @@
+﻿
+namespace Tx.Network.Snmp.Dynamic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Net;
+    using System.Reactive;
+    using System.Reflection;
+
+    /// <summary>
+    /// TypeMap implementation for SNMP attributed classes.
+    /// </summary>
+    public sealed class TrapTypeMap : IPartitionableTypeMap<IpPacket, ObjectIdentifier>
+    {
+        private readonly static ObjectIdentifier trapOid = new ObjectIdentifier("1.3.6.1.6.3.1.1.4.1.0");
+
+        public TrapTypeMap()
+        {
+            this.TimeFunction = packet => packet.ReceivedTime;
+            this.Comparer = EqualityComparer<ObjectIdentifier>.Default;
+        }
+
+        public Func<IpPacket, DateTimeOffset> TimeFunction { get; private set; }
+
+        public IEqualityComparer<ObjectIdentifier> Comparer { get; private set; }
+
+        public Func<IpPacket, object> GetTransform(Type outputType)
+        {
+            return CreateTransform(outputType);
+        }
+
+        public ObjectIdentifier GetTypeKey(Type outputType)
+        {
+            var attribute = outputType.GetAttribute<SnmpTrapAttribute>();
+            return attribute != null ? attribute.SnmpTrapOid : default(ObjectIdentifier);
+        }
+
+        public ObjectIdentifier GetInputKey(IpPacket evt)
+        {
+            var snmpDatagram = GetSnmpDatagram(evt);
+            if (snmpDatagram == default(SnmpDatagram))
+            {
+                return default(ObjectIdentifier);
+            }
+
+            var pdu = snmpDatagram.PDU;
+            if (pdu.PduType != PduType.SNMPv2Trap || pdu.VarBinds == null)
+            {
+                return default(ObjectIdentifier);
+            }
+
+            VarBind trapVarBind;
+            return pdu.SearchFirstSubOidWith(trapOid, out trapVarBind)
+                       ? (ObjectIdentifier)trapVarBind.Value
+                       : default(ObjectIdentifier);
+        }
+
+        internal static Func<IpPacket, object> CreateTransform(Type outputTrapType)
+        {
+            if (outputTrapType.GetAttribute<SnmpTrapAttribute>() == null)
+            {
+                return null;
+            }
+
+            var parameter = Expression.Parameter(typeof(IpPacket), "ipPacket");
+            var getPduCall = Expression.Call(typeof(TrapTypeMap).GetMethod("GetPdu", BindingFlags.Static | BindingFlags.NonPublic), parameter);
+
+            var pduVar = Expression.Variable(typeof(SnmpPDU), "pdu");
+            var assignment = Expression.Assign(pduVar, getPduCall);
+            var pduVarBindsField = typeof(SnmpPDU).GetField(
+                "VarBinds",
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+            var sourceAddressProperty = typeof(IpPacket).GetProperty(
+                "SourceIpAddress",
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+
+            var varbindVar = Expression.Variable(typeof(VarBind), "varBind");
+            var varbindValueField = typeof(VarBind).GetField("Value", BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+
+            var getVarBindMethod = typeof(SnmpPDU).GetMethod("SearchFirstSubOidWith");
+            var bindings = new List<MemberBinding>();
+
+            MemberAssignment notificationObjectsExpression = null, ipAddressExpresion = null;
+            foreach (var p in outputTrapType.GetProperties())
+            {
+                var notificationObjectIdentifier =
+                    p.GetCustomAttributes(typeof(SnmpOidAttribute), false).OfType<SnmpOidAttribute>().FirstOrDefault();
+                if (notificationObjectIdentifier == null)
+                {
+                    var notificationObjects =
+                        p.GetCustomAttributes(typeof(NotificationObjectsAttribute), false)
+                            .OfType<NotificationObjectsAttribute>()
+                            .FirstOrDefault();
+                    if (notificationObjects != null && p.PropertyType.IsAssignableFrom(pduVarBindsField.FieldType))
+                    {
+                        notificationObjectsExpression = Expression.Bind(p, Expression.Field(pduVar, pduVarBindsField));
+                    }
+
+                    var ipAddressAttribute =
+                    p.GetCustomAttributes(typeof(IpAddressAttribute), false)
+                        .OfType<IpAddressAttribute>()
+                        .FirstOrDefault();
+                    if (ipAddressAttribute != null)
+                    {
+                        Expression ipAddress = Expression.Property(parameter, sourceAddressProperty);
+                        if (p.PropertyType == typeof(string))
+                        {
+                            ipAddress = Expression.Call(ipAddress, typeof(IPAddress).GetMethod("ToString"));
+                        }
+
+                        ipAddressExpresion = Expression.Bind(p, ipAddress);
+                    }
+
+                    continue;
+                }
+
+                var foundValue = Expression.Call(pduVar, getVarBindMethod, Expression.Constant(notificationObjectIdentifier.Oid), varbindVar);
+
+                Expression convertedValue = Expression.Field(varbindVar, varbindValueField);
+                if (p.PropertyType.IsEnum || typeof(int).IsAssignableFrom(p.PropertyType))
+                {
+                    convertedValue = Expression.Convert(convertedValue, typeof(int));
+                }
+
+                var conditional = Expression.Condition(foundValue, Expression.Convert(convertedValue, p.PropertyType),
+                    Expression.Default(p.PropertyType));
+                var b = Expression.Bind(p, conditional);
+                bindings.Add(b);
+            }
+
+            if (notificationObjectsExpression != null)
+            {
+                bindings.Add(notificationObjectsExpression);
+            }
+
+            if (ipAddressExpresion != null)
+            {
+                bindings.Add(ipAddressExpresion);
+            }
+
+            var newExpression = Expression.New(outputTrapType);
+            var memberInitialization = Expression.MemberInit(newExpression, bindings.ToArray());
+            var castToObject = Expression.Convert(memberInitialization, typeof(object));
+
+            var nullCheck = Expression.Condition(
+                Expression.Equal(Expression.Constant(null, typeof(IpPacket)), parameter),
+                Expression.Constant(null, typeof(object)), castToObject);
+
+            var codeBlock = Expression.Block(new[] { pduVar, varbindVar, }, assignment, nullCheck);
+            var transformExpression = Expression.Lambda<Func<IpPacket, object>>(codeBlock, parameter);
+
+            return transformExpression.Compile();
+        }
+
+        public static SnmpDatagram GetSnmpDatagram(IpPacket ipPacket)
+        {
+            var udpDatagram = ipPacket as UdpDatagram;
+            if (udpDatagram == default(UdpDatagram))
+            {
+                return default(SnmpDatagram);
+            }
+
+            try
+            {
+                return udpDatagram.TryParseSnmpDatagram();
+            }
+            catch
+            {
+                return default(SnmpDatagram);
+            }
+        }
+
+        private static SnmpPDU GetPdu(IpPacket ipPacket)
+        {
+            var snmpDatagram = GetSnmpDatagram(ipPacket);
+            return snmpDatagram != default(SnmpDatagram) ? snmpDatagram.PDU : default(SnmpPDU);
+        }
+    }
+}
