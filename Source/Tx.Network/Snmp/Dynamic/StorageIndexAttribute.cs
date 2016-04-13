04a450155384d8aed7d458ce30e62f4bc8fe061a
sanjaysurfer <sanjaysurfer@gmail.com>
2016-04-13 11:44:27 -0700

    Signed-off-by: sanjaysurfer <sanjaysurfer@gmail.com>

---
+++ Source/Tx.Network/Snmp/Dynamic/StorageIndexAttribute.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Tx.Network.Snmp.Dynamic
+{
+    using System;
+
+    /// <summary>
+    /// Attribute used to mark property of an MIB table as index for storage.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class StorageIndexAttribute : Attribute
+    {
+    }
+}
