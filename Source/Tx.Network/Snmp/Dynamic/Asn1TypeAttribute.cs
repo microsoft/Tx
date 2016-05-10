1183278344538459669fb27c28dea2e9a43d995f
sansinh <sansinh@MININT-97T8LH1>
2016-05-10 14:52:30 -0700

    adding proj files to the chnages
    Signed-off-by: Sanjay Kumar Sinha <sansinh@microsoft.com>
    Signed-off-by: sansinh <sansinh@MININT-97T8LH1>

--- Source/Tx.Network/Snmp/Dynamic/Asn1TypeAttribute.cs
+++ Source/Tx.Network/Snmp/Dynamic/Asn1TypeAttribute.cs
@@ -11,9 +11,24 @@ namespace Tx.Network.Snmp.Dynamic
     {
         public readonly Asn1TagInfo tagInfo;
 
-        public Asn1TypeAttribute(Asn1TagInfo asn1TagInfo)
+        public Asn1TypeAttribute(Asn1Tag asn1TagInfo)
         {
-            this.tagInfo = asn1TagInfo;
+            this.tagInfo = new Asn1TagInfo(asn1TagInfo);
+        }
+
+        public Asn1TypeAttribute(Asn1SnmpTag asn1SnmpTag)
+        {
+            this.tagInfo = new Asn1TagInfo(asn1SnmpTag);
+        }
+
+        public Asn1TypeAttribute(Asn1SnmpTag asn1SnmpTag, ConstructType constructType)
+        {
+            this.tagInfo = new Asn1TagInfo(asn1SnmpTag, constructType);
+        }
+
+        public Asn1TypeAttribute(Asn1Tag asn1Tag, ConstructType constructType, Asn1Class asn1Class)
+        {
+            this.tagInfo = new Asn1TagInfo(asn1Tag, constructType, asn1Class);
         }
     }
 }
