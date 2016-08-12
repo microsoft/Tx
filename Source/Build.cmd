6c4a0fee4fec724632b3b74df724f3268df3978c
Sergey Baranchenkov <tuatan@yandex.ru>
2016-08-11 18:58:48 -0700

    Implemented fail-fast behavior for build script

--- Source/Build.cmd
+++ Source/Build.cmd
@@ -1,30 +1,36 @@
 set bin="c:\Bin"
 
+if not exist "%bin%" mkdir %bin% || goto failFast
+
+pushd %~dp0
+
 msbuild /p:Configuration=Release40
 msbuild /p:Configuration=Debug40
 msbuild /p:Configuration=Release45
 msbuild /p:Configuration=Debug45
 
-copy ..\tools\NuGet.exe %bin%\
-copy ..\tools\zip.exe %bin%\
-copy ..\tools\PushPackages.cmd %bin%\
+popd
 
-pushd  ..\Samples\LinqPad\Queries
+copy ..\tools\NuGet.exe %bin%\ || goto failFast
+copy ..\tools\zip.exe %bin%\ || goto failFast
+copy ..\tools\PushPackages.cmd %bin%\ || goto failFast
+
+pushd  ..\Samples\LinqPad\Queries || goto failFast
 call create_samples_package.cmd
 popd
 
 pushd
 
-cd /d %bin%\Debug
-call :setVersion
-call :packAll
+cd /d %bin%\Debug || goto failFast
+call :setVersion || goto failFast
+call :packAll || goto failFast
 
-cd /d %bin%\Release
-call :setVersion
-call :packAll
+cd /d %bin%\Release || goto failFast
+call :setVersion || goto failFast
+call :packAll || goto failFast
 
-cd /d %bin%\Release\Net40
-..\..\zip.exe ..\..\Tx.LinqPad.lpx header.xml System.Reactive.Interfaces.dll System.Reactive.Core.dll System.Reactive.Linq.dll System.Reactive.PlatformServices.dll System.Reactive.Windows.Forms.dll Tx.Core.dll Tx.Windows.dll Tx.Windows.TypeGeneration.dll Tx.SqlServer.dll msvcr100.dll xe.dll Microsoft.SqlServer.XE.Core.dll Microsoft.SqlServer.XEvent.Configuration.dll Microsoft.SqlServer.XEvent.dll Microsoft.SqlServer.XEvent.Linq.dll Microsoft.SqlServer.XEvent.Targets.dll Tx.LinqPad.dll HTTP_Server.man HTTP_Server.etl BasicPerfCounters.blg CrossMachineHTTP.etl CrossMachineIE.etl IE_Client.man sqltrace.xel Microsoft.Windows.ApplicationServer.Applications.man SampleWcfTrace.etl
+cd /d %bin%\Release\Net40 || goto failFast
+..\..\zip.exe ..\..\Tx.LinqPad.lpx header.xml System.Reactive.Interfaces.dll System.Reactive.Core.dll System.Reactive.Linq.dll System.Reactive.PlatformServices.dll System.Reactive.Windows.Forms.dll Tx.Core.dll Tx.Windows.dll Tx.Windows.TypeGeneration.dll Tx.SqlServer.dll msvcr100.dll xe.dll Microsoft.SqlServer.XE.Core.dll Microsoft.SqlServer.XEvent.Configuration.dll Microsoft.SqlServer.XEvent.dll Microsoft.SqlServer.XEvent.Linq.dll Microsoft.SqlServer.XEvent.Targets.dll Tx.LinqPad.dll HTTP_Server.man HTTP_Server.etl BasicPerfCounters.blg CrossMachineHTTP.etl CrossMachineIE.etl IE_Client.man sqltrace.xel Microsoft.Windows.ApplicationServer.Applications.man SampleWcfTrace.etl || goto failFast
 
 
 popd
@@ -32,37 +38,41 @@ goto end
 
 :setVersion
 
-pushd Net40\Properties
-..\SetVersion.exe
+pushd Net40\Properties || goto failFast
+..\SetVersion.exe || goto failFast
 popd
 
-pushd Net45\Properties
-..\SetVersion.exe
+pushd Net45\Properties || goto failFast
+..\SetVersion.exe || goto failFast
 popd
 
-exit /b 0
+goto end
 
 :packAll
-call :pack Tx.Core
-call :pack Tx.Windows
-call :pack Tx.Bond
-call :pack Tx.Network
-call :pack Tx.Windows.TypeGeneration
-call :pack Tx.SqlServer
-call :pack Tx.All
+call :pack Tx.Core || goto failFast
+call :pack Tx.Windows || goto failFast
+call :pack Tx.Bond || goto failFast
+call :pack Tx.Network || goto failFast
+call :pack Tx.Windows.TypeGeneration || goto failFast
+call :pack Tx.SqlServer || goto failFast
+call :pack Tx.All || goto failFast
 
-exit /b 0
+goto end
 
 :pack %1
-call Net40\Properties\%1.Layout.cmd
-cd /d %1
-copy ..\Net40\Properties\%1.nuspec
-..\..\NuGet pack %1.nuspec
-move *.nupkg ..\
+call Net40\Properties\%1.Layout.cmd || goto failFast
+cd /d %1 || goto failFast
+copy ..\Net40\Properties\%1.nuspec || goto failFast
+..\..\NuGet pack %1.nuspec || goto failFast
+move *.nupkg ..\ || goto failFast
 cd ..
-rd /s/q %1
-exit /b 0
+rd /s/q %1 || goto failFast
+goto end
 
 :end
-popd
+cd %~dp0
 exit /b 0
+
+:failFast
+cd %~dp0
+exit /b 1
