ce8cef8ebc94ea0885bb274c3f81bb2c3246ad4f
RobThree <rob@devcorner.nl>
2017-04-04 13:42:37 +0200

    Added ReadStream method so a PCAP stream can be read from anywhere; not requiring a file.

--- Source/Tx.Network/Pcap/Pcap.cs
+++ Source/Tx.Network/Pcap/Pcap.cs
@@ -15,51 +15,56 @@ namespace Tx.Network
         {
             using (var stream = File.OpenRead(filename))
             {
-                using (var reader = new BinaryReader(stream))
+                return ReadStream(stream);
+            }
+        }
+
+        public static IEnumerable<PcapRecord> ReadStream(Stream stream)
+        {
+            using (var reader = new BinaryReader(stream))
+            {
+                int pos = 0;
+                int length = (int)reader.BaseStream.Length;
+                if (length <= (24 + 16))
                 {
-                    int pos = 0;
-                    int length = (int)reader.BaseStream.Length;
-                    if (length <= (24 + 16))
+                    yield break;
+                }
+
+                var magicNumber = reader.ReadUInt32();
+                var version_major = reader.ReadUInt16();
+                var version_minor = reader.ReadUInt16();
+                var thiszone = reader.ReadInt32();
+                var sigfigs = reader.ReadUInt32();
+                var snaplen = reader.ReadUInt32();
+                var network = reader.ReadUInt32();
+
+                pos += 24;
+
+                while ((pos + 16) < length)
+                {
+                    var ts_sec = reader.ReadUInt32();
+                    var ts_usec = reader.ReadUInt32();
+                    var incl_len = reader.ReadUInt32();
+                    var orig_len = reader.ReadUInt32();
+
+                    pos += 16;
+
+                    if ((pos + incl_len) > length)
                     {
                         yield break;
                     }
 
-                    var magicNumber = reader.ReadUInt32();
-                    var version_major = reader.ReadUInt16();
-                    var version_minor = reader.ReadUInt16();
-                    var thiszone = reader.ReadInt32();
-                    var sigfigs = reader.ReadUInt32();
-                    var snaplen = reader.ReadUInt32();
-                    var network = reader.ReadUInt32();
+                    var data = reader.ReadBytes((int)incl_len);
+                    pos += (int)incl_len;
 
-                    pos += 24;
-
-                    while ((pos + 16) < length)
+                    yield return new PcapRecord
                     {
-                        var ts_sec = reader.ReadUInt32();
-                        var ts_usec = reader.ReadUInt32();
-                        var incl_len = reader.ReadUInt32();
-                        var orig_len = reader.ReadUInt32();
-
-                        pos += 16;
-
-                        if ((pos + incl_len) > length)
-                        {
-                            yield break;
-                        }
-
-                        var data = reader.ReadBytes((int)incl_len);
-                        pos += (int)incl_len;
-
-                        yield return new PcapRecord
-                        {
-                            Data = data,
-                            NetworkId = network,
-                            Timestamp = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)
-                                .AddSeconds(ts_sec + thiszone)
-                                .AddMilliseconds((ts_usec == 0 || ts_usec >= 1000000) ? ts_usec : ts_usec / 1000)
-                        };
-                    }
+                        Data = data,
+                        NetworkId = network,
+                        Timestamp = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)
+                            .AddSeconds(ts_sec + thiszone)
+                            .AddMilliseconds((ts_usec == 0 || ts_usec >= 1000000) ? ts_usec : ts_usec / 1000)
+                    };
                 }
             }
         }
