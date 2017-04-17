398d013d4cb79dd711a80148db9cba41dc2e6cc1
georgis <georgis@microsoft.com>
2017-04-17 09:05:58 -0700

    Rx sample for mouse move events

---
+++ Samples/RxWinforms/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace RxWinforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(576, 341);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 341);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Samples/RxWinforms/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Reactive.Linq;
+using System.Windows.Forms;
+
+namespace RxWinforms
+{
+    // This sample illustrates the original usage of Reactive Extensions for event-driven UI
+    // Good book with more examples is "Programming Reactive Extensions and LINQ" http://www.apress.com/9781430237471
+    public partial class Form1 : Form
+    {
+        IDisposable _subscription;
+        public Form1()
+        {
+            InitializeComponent();
+
+            var pen = new Pen(Color.Black, 2);
+
+            var points = Observable.FromEventPattern<MouseEventArgs>(panel1, "MouseMove")
+                              .Where(m => m.EventArgs.Button == MouseButtons.Left)
+                              //.Sample(TimeSpan.FromMilliseconds(100))   // Un-comment this to pass only 1 event per 100 ms
+                              .Select(m => new Point(m.EventArgs.X, m.EventArgs.Y));
+
+            _subscription = points.Subscribe(p =>
+            {
+                panel1.CreateGraphics().FillRectangle(pen.Brush, p.X, p.Y, 5, 5);
+            });
+        }
+    }
+}
