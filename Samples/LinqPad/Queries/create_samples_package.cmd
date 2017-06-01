08f0808f07c3c7c1be2524e43c8b82a7c9e36cc9
REDMOND\sergebar <sergebar@microsoft.com>
2017-05-31 22:27:04 -0700

    Fixed create samples cmd

--- Samples/LinqPad/Queries/create_samples_package.cmd
+++ Samples/LinqPad/Queries/create_samples_package.cmd
@@ -1,2 +1,2 @@
-del c:\bin\samples.zip 
-..\..\..\Tools\zip -r c:\bin\samples.zip header.xml NoManifest HTTP.sys IE_IIS "Performance Counters" SqlXevent WcfTroubleshooting
+if exist %1 del %1 || goto failFast 
+..\..\..\Tools\zip -r %1 header.xml NoManifest HTTP.sys IE_IIS "Performance Counters" SqlXevent WcfTroubleshooting
