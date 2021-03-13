1902666bbb591c084e2f38bd8cfaff46353952a8
arpitadessai <arpita_dessai@live.com>
2021-03-12 16:50:21 -0800

    Handle "array bounds are invalid" exception. (#64)

---
+++ Source/Tx.Windows.Logs/EventLogReaderExceptions.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+namespace Tx.Windows
+{
+    public static class EventLogReaderExceptions
+    {
+        public static string ArrayBoundsInvalid = "The array bounds are invalid";
+    }
+}
